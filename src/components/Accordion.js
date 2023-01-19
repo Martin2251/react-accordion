785bb828ac20891d514b69eb76e7bf4aa0d499a5
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-01-19 17:59:47 +0000

    data

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import "./accordion.css";
+import {AiOutlinePlusCircle, AiOutlineMinusCircle} from "react-icons/ai"
 
 const Accordion = ({ heading, content }) => {
   const [isActive, setIsActive] = useState(false);
   return (
     <li className="accordion-item">
       <div className="accordion-toggle" onClick={() => setIsActive(!isActive)}>
-      <span>{isActive ? "-" : "+"}</span><h3>{heading}</h3><span>{isActive ? "-" : "+"}</span>
+      <span>{isActive ? <AiOutlineMinusCircle /> : <AiOutlinePlusCircle  />}</span><h3>{heading}</h3><span>{isActive ? "-" : "+"}</span>
       </div>
       {isActive && <div className="accordion-content">{content}</div>}
     </li>
